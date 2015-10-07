d712d32cb8d444abfafae6539419644c1422674e
Petio Petev <petio.petev@gmail.com>
2015-10-07 16:27:50 +0300

    add scripts for building docker image for gorouter

---
+++ example/docker_gorouter/build_gorouter_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ~/work/src/github.com/cloudfoundry/gorouter/gorouter .
+sudo docker build -t ppetev/gorouter .
---
+++ example/docker_gorouter/do_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./install_docker.sh
+./compile_gorouter.sh
+./build_gorouter_docker.sh
